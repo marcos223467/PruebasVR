b9aabd14e2910bcdf24b2afa010a9557e2e8d12c
marcos223467 <marcos223467@gmail.com>
2023-01-03 20:28:49 +0100

    Puesta la puerta, intentar hacer lumos

--- Assets/Scripts/Varita.cs
+++ Assets/Scripts/Varita.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.OpenXR.Input;
 
 public class Varita : MonoBehaviour
 {
@@ -12,17 +14,36 @@ public class Varita : MonoBehaviour
     private bool volver;
 
     public float speed;
+
+    private InputDevice device;
+
+    public GameObject luz;
+
+    private bool activaLuz;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        /*List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+            device = devices[0];
+        
+        rb = GetComponent<Rigidbody>();*/
         volver = false;
+        activaLuz = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*device.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        device.TryGetFeatureValue(CommonUsages.trigger, out float trig);
+        if (primaryButtonValue || trig > 0.1)
+            activaLuz = true;
+
+        luz.SetActive(activaLuz);*/
     }
 
     public void FixedUpdate()
@@ -45,7 +66,7 @@ public class Varita : MonoBehaviour
         else
         {
             Vector3 dir = (attach.position-transform.position).normalized;
-            rb.AddForce(dir * speed, ForceMode.Force);
+            rb.AddForce(dir.normalized * speed, ForceMode.Force);
         }
         
     }
